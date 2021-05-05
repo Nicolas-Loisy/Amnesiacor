104a1986d1fa1b80809b15da7d6352ed9160fd20
Bakili Shaina <shaina3322@hotmail.com>
2021-05-05 22:03:16 +0200

    mouvehandlev1

--- src/application/controleur/Controleur.java
+++ src/application/controleur/Controleur.java
@@ -5,16 +5,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.sun.javafx.geom.Rectangle;
+import com.sun.scenario.effect.impl.state.LinearConvolveKernel;
 
 import application.Main;
 import application.modele.Environnement;
 import application.modele.Link;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -30,15 +34,12 @@ public class Controleur implements Initializable {
 	@FXML
     private TilePane TileMap;
 
-
+	//handlerScene
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		word = new Environnement(50, 50);
-		
-
-		
 		
 		link = new Link(15, 15, "A");//crea link modele
 		
@@ -53,42 +54,30 @@ public class Controleur implements Initializable {
 		linkVue.translateYProperty().bind(link.getyProporty());
 		
 		TileMap.getChildren().add(linkVue);//add du link dans la map
+
 		
-		TileMap.setOnMousePressed(e -> System.out.println("hello"));
-		
-		
-		
-		TileMap.setOnKeyPressed(e -> { 
-			
-			System.out.println("LOL");
-			
-			
-			if(e.getCode() == KeyCode.Z) {
-				System.out.println("Z");
-				link.setY(link.getY()-1);
-			}
-			else if (e.getCode() == KeyCode.S){
-				System.out.println("S");
-				link.setY(link.getY()+1);
-			}
-			else if (e.getCode() == KeyCode.D){
-				System.out.println("D");
-				link.setX(link.getX()+1);
-			}
-			else if (e.getCode() == KeyCode.Q){
-				System.out.println("Q");
-				link.setX(link.getX()-1);
-			}
-			
-			
-		});
-		
-		System.out.println(TileMap.getScene()+ "lol");
 		
 	}
 	
-	public void creaLink(Link link){
-		Rectangle r = new Rectangle();
+	@FXML
+	void moveHandle(MouseEvent event) {
+		System.out.println("heree");
+		TileMap.getScene().setOnKeyPressed(e->{
+			
+			if(e.getCode() == KeyCode.Z) {
+				link.setY(link.getY()-10);
+			}
+			else if (e.getCode() == KeyCode.S){
+				link.setY(link.getY()+10);
+			}
+			else if (e.getCode() == KeyCode.D){
+				link.setX(link.getX()+10);
+			}
+			else if (e.getCode() == KeyCode.Q){
+				link.setX(link.getX()-10);
+			}
+			
+		});
 	}
 
 }
